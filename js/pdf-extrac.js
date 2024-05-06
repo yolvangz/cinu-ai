df714edf4933cb36fcb08ca5f2ceb9c5b865c49d
Luis C. <Luismarval5a@hotmail.com>
2024-05-06 15:30:55 -0400

    Ahora funciona a traves de async await en lugar de .then

--- js/pdf-extrac.js
+++ js/pdf-extrac.js
@@ -1,13 +1,28 @@
-const fs = require('node:fs');
+const fs = require('node:fs/promises');
 const pdf = require('pdf-parse');
 
 
-let databuffer = fs.readFileSync("ruta del archivo.pdf");
-
-pdf(databuffer).then((data) => {
-    let info = data.text; 
-    console.log(info);
-})
-.catch((error) => {
-    console.log(error)
-}) 
+const databuffer = async () => {
+    try{
+        const text = await fs.readFile(".\\js\\cinu.pdf");
+        const buffer = Buffer.from(text);
+        return buffer;
+    }catch(err){
+        if(err.code === "ENOENT"){
+            console.log("Error: archivo no encontrado");
+        }else{
+            console.log("Error al leer el archivo:",err);
+        }
+        throw err;
+    }
+}
+const read = async () =>{
+    try{
+        const info = await databuffer();
+        const data = await pdf(info);
+        console.log(data.text);
+    }catch(err){
+        console.log("ha ocurrido el siguiente error",err.code);
+    }
+}
+read()
