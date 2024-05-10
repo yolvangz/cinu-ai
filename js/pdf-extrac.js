d22178babbe8f75d414bc17dacf2f848f26dbbbc
Luis C. <Luismarval5a@hotmail.com>
2024-05-10 16:54:13 -0400

    Ahora los pdf se convierten en chunks

--- js/pdf-extrac.js
+++ js/pdf-extrac.js
@@ -3,26 +3,34 @@ const pdf = require('pdf-parse');
 
 
 const databuffer = async () => {
-    try{
-        const text = await fs.readFile(".\\js\\cinu.pdf");
-        const buffer = Buffer.from(text);
-        return buffer;
-    }catch(err){
-        if(err.code === "ENOENT"){
-            console.log("Error: archivo no encontrado");
-        }else{
-            console.log("Error al leer el archivo:",err);
+  try{
+    const text = await fs.readFile("js/cinu.pdf");
+    const buffer = Buffer.from(text);
+    return buffer;  
+  }catch(err){
+    if(err.code === "ENOENT"){
+        console.log("Error: archivo no encontrado");
+      }else{
+        console.log("Error al leer el archivo:",err);
         }
-        throw err;
+      throw err;
     }
 }
 const read = async () =>{
-    try{
-        const info = await databuffer();
-        const data = await pdf(info);
-        console.log(data.text);
-    }catch(err){
-        console.log("ha ocurrido el siguiente error",err.code);
-    }
+    const info = await databuffer();
+    const data = await pdf(info);
+    console.log(data.text);
+    return data.text;
 }
-read()
+const chunk = async () => {
+  const contenido = await read();
+  const tamano = 10000;
+  const desplazamiento = 9000;
+  const chunks = [];
+  for (let i = 0; i < contenido.length; i += desplazamiento) {
+      let trozo = contenido.substring(i, i + tamano);
+      chunks.push(trozo);
+  }
+  console.log(chunks)
+}
+chunk();
