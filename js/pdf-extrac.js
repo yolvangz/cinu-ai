16bda34aef204a65f066cb08f76452ddd63dd834
Yolvan G. Zambrano <yolvanzg@gmail.com>
2024-05-14 12:10:17 -0400

    Set up Prettier configuration for standar format with Prettier extension for VS Code

--- js/pdf-extrac.js
+++ js/pdf-extrac.js
@@ -1,36 +1,35 @@
-const fs = require('node:fs/promises');
-const pdf = require('pdf-parse');
-
+const fs = require("node:fs/promises");
+const pdf = require("pdf-parse");
 
 const databuffer = async () => {
-  try{
-    const text = await fs.readFile("js/cinu.pdf");
-    const buffer = Buffer.from(text);
-    return buffer;  
-  }catch(err){
-    if(err.code === "ENOENT"){
-        console.log("Error: archivo no encontrado");
-      }else{
-        console.log("Error al leer el archivo:",err);
-        }
-      throw err;
-    }
-}
-const read = async () =>{
-    const info = await databuffer();
-    const data = await pdf(info);
-    console.log(data.text);
-    return data.text;
-}
+	try {
+		const text = await fs.readFile("js/cinu.pdf");
+		const buffer = Buffer.from(text);
+		return buffer;
+	} catch (err) {
+		if (err.code === "ENOENT") {
+			console.log("Error: archivo no encontrado");
+		} else {
+			console.log("Error al leer el archivo:", err);
+		}
+		throw err;
+	}
+};
+const read = async () => {
+	const info = await databuffer();
+	const data = await pdf(info);
+	console.log(data.text);
+	return data.text;
+};
 const chunk = async () => {
-  const contenido = await read();
-  const tamano = 10000;
-  const desplazamiento = 9000;
-  const chunks = [];
-  for (let i = 0; i < contenido.length; i += desplazamiento) {
-      let trozo = contenido.substring(i, i + tamano);
-      chunks.push(trozo);
-  }
-  console.log(chunks)
-}
+	const contenido = await read();
+	const tamano = 10000;
+	const desplazamiento = 9000;
+	const chunks = [];
+	for (let i = 0; i < contenido.length; i += desplazamiento) {
+		let trozo = contenido.substring(i, i + tamano);
+		chunks.push(trozo);
+	}
+	console.log(chunks);
+};
 chunk();
