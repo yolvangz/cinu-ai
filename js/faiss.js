3ddbdde74e8c39a81e63aaa9a9da6d65bd004a52
Luis C. <Luismarval5a@hotmail.com>
2024-05-14 15:53:30 -0400

    atras

--- js/faiss.js
+++ js/faiss.js
@@ -5,7 +5,6 @@ const { PromptTemplate } = require("@langchain/core/prompts")
 const { loadQAStuffChain } = require("langchain/chains")
 const { FaissStore } = require("@langchain/community/vectorstores/faiss")
 const { chunk } = require("./pdf-extrac")
-const { argv } = require("node:process")
 
 const get_vector_store = async () => {
   let text = await chunk();
@@ -16,18 +15,10 @@ const get_vector_store = async () => {
   return await vector_store.save("faiss_index")
 }
 const conversational_chain = async () =>{
-
-  const prompt_template = `
-  Answer the question as detailed as possible from the provided context, make sure to provide all the details, if the answer is not in
-  provided context just say, "answer is not available in the context", don't provide the wrong answer\n\n
-  Context:\n {context}?\n
-  Question: \n{question}\n
-  Answer:
-  `
-  const model = new ChatGoogleGenerativeAI({apiKey: "AIzaSyA1KvMEhxY5fk0ObK9_OYNC0ZxgKfYLj5A",emperature: 0.3,modelName: "gemini-pro"})
-  
+  const prompt_template = "Answer the question as detailed as possible from the provided context, make sure to provide all the details, if the answer is not inprovided context just say, 'answer is not available in the context', don't provide the wrong answer\n\nContext:\n {context}?\n Question: \n{question}\nAnswer:"
+  const model = new ChatGoogleGenerativeAI({apiKey: "AIzaSyA1KvMEhxY5fk0ObK9_OYNC0ZxgKfYLj5A",temperature: 0.3,modelName: "gemini-pro"})
   const prompt = new PromptTemplate({inputVariables:["context", "question"],template:prompt_template})
-  const chain = prompt.pipe(model)
+  const chain = loadQAStuffChain(llm=model,params=prompt)
 
   return chain
 }
@@ -37,16 +28,14 @@ const user_input = async (user_question) =>{
   const new_db = await FaissStore.load("faiss_index", embeddings)
   const docs = await new_db.similaritySearch(user_question)
   const chain = await conversational_chain()
-  /*const response = await chain.prepOutputs({inputs:docs, outputs:user_question , returnOnlyOutputs:true})
+  const response = await chain.invoke({inputs:user_question})
   console.log(response);
-  console.log("Reply: ", response["output_text"]); */
-/*   const response = chain.invoke(user_question);
-  console.log(response) */
+  /* console.log("Reply: ", response["output_text"]);  */
 }
 
 const main = () =>{
   console.log("Chat with PDF using Gemini")
-  user_question = argv
+  user_question ="Comunicación y convivencia"
   if (user_question){
     user_input(user_question[2])
   }
