83d6b53cbcfe01553ec0078ad96f674a0435fd34
Luis C. <Luismarval5a@hotmail.com>
2024-05-14 15:04:00 -0400

    prueba

--- js/faiss.js
+++ js/faiss.js
@@ -11,6 +11,7 @@ const get_vector_store = async () => {
   let text = await chunk();
   let metadata = [{"name":"archivo"}]
   let embedding = new GoogleGenerativeAIEmbeddings({apiKey: "AIzaSyA1KvMEhxY5fk0ObK9_OYNC0ZxgKfYLj5A",modelName: "embedding-001"});;
+  console.log(text)
   let vector_store = await FaissStore.fromTexts(text,metadata,embedding)
   return await vector_store.save("faiss_index")
 }
@@ -26,7 +27,7 @@ const conversational_chain = async () =>{
   const model = new ChatGoogleGenerativeAI({apiKey: "AIzaSyA1KvMEhxY5fk0ObK9_OYNC0ZxgKfYLj5A",emperature: 0.3,modelName: "gemini-pro"})
   
   const prompt = new PromptTemplate({inputVariables:["context", "question"],template:prompt_template})
-  const chain = loadQAStuffChain(llm=model,params=prompt)
+  const chain = prompt.pipe(model)
 
   return chain
 }
@@ -36,17 +37,18 @@ const user_input = async (user_question) =>{
   const new_db = await FaissStore.load("faiss_index", embeddings)
   const docs = await new_db.similaritySearch(user_question)
   const chain = await conversational_chain()
-  
-  const response = chain.prepOutputs({inputs:docs, outputs:user_question , returnOnlyOutputs:true})
-  console.log(response)
-  console.log("Reply: ", response["output_text"])
+  /*const response = await chain.prepOutputs({inputs:docs, outputs:user_question , returnOnlyOutputs:true})
+  console.log(response);
+  console.log("Reply: ", response["output_text"]); */
+/*   const response = chain.invoke(user_question);
+  console.log(response) */
 }
 
 const main = () =>{
   console.log("Chat with PDF using Gemini")
   user_question = argv
   if (user_question){
-      user_input(user_question[2])
-    }
+    user_input(user_question[2])
   }
-  main()
+}
+main()
