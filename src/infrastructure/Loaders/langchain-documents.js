7bde951308cdc3d6536b895f6c9fb43d65f12ebd
Yolvan G. Zambrano <yolvanzg@gmail.com>
2024-05-21 21:58:24 -0400

    Added module that supports load of documents and chunk it into text chunks or document objects from langchain framework as well.

---
+++ src/infrastructure/Loaders/langchain-documents.js
@@ -0,0 +1,83 @@
+const fs = require("node:fs");
+const path = require("node:path");
+const { Document } = require("langchain/document");
+const { DirectoryLoader } = require("langchain/document_loaders/fs/directory");
+const { PDFLoader } = require("langchain/document_loaders/fs/pdf");
+const { DocxLoader } = require("langchain/document_loaders/fs/docx");
+const { RecursiveCharacterTextSplitter } = require("langchain/text_splitter");
+
+class DocumentsLoader {
+	constructor() {
+		this.fileSystem = fs;
+	}
+	#accessFolder(uri) {
+		return this.fileSystem.access(uri);
+	}
+	#pdf(path) {
+		return new PDFLoader(path, { splitPages: false });
+	}
+	#docx(path) {
+		return new DocxLoader(path);
+	}
+	#trim(text) {
+		const trimData = text.trim();
+		const lines = trimData.split(/\s*\n\s*/);
+		const trimmedLines = lines.map((line) => line.trim());
+		return trimmedLines.join("\n");
+	}
+	async readFile(uri, docObject = false) {
+		let loader;
+		switch (path.extname(uri)) {
+			case ".pdf":
+				loader = this.#pdf(uri);
+				break;
+			case ".docx":
+				loader = this.#docx(uri);
+				break;
+			default:
+				throw new Error("extension not supported");
+		}
+		const result = await loader.load();
+		const doc = result[0];
+		doc.pageContent = this.#trim(doc.pageContent);
+		return docObject ? doc : doc.pageContent;
+	}
+	async readFolder(documentsFolder, docObject = false) {
+		if (!this.#accessFolder(documentFolder))
+			throw new Error(`No existe el directorio ${this.documentFolder}`);
+		const directoryLoader = new DirectoryLoader(documentFolder, {
+			".pdf": this.#pdf,
+			".docx": this.#docx,
+		});
+		documentsObjects = await this.directoryLoader.load();
+		return docObject
+			? documentsObjects
+			: documentsObjects.map((doc) => doc.pageContent);
+	}
+	async chunk(input, chunkSize, chunkOverlap, docObject = false) {
+		let documents;
+		const textSplitter = new RecursiveCharacterTextSplitter({
+			chunkSize: chunkSize,
+			chunkOverlap: chunkOverlap,
+		});
+		if (Array.isArray(input)) {
+			if (input.every((doc) => doc instanceof Document)) {
+				documents = input;
+			} else if (input.every((doc) => typeof doc === "string")) {
+				documents = input.map((doc) => new Document({ pageContent: doc }));
+			} else {
+				throw new Error("input must be an array of strings or documents");
+			}
+		} else {
+			documents =
+				input instanceof Document
+					? [input]
+					: [new Document({ pageContent: input })];
+		}
+		const result = await textSplitter.splitDocuments(documents);
+		if (docObject) return result;
+		return result.map((doc) => doc.pageContent);
+	}
+}
+
+module.exports = DocumentsLoader;
---
+++ tests/src/langchain-documents.test.js
@@ -0,0 +1,76 @@
+const fs = require("fs");
+const dotenv = require("dotenv");
+const dir = require("../../lib/dir.js");
+const { implementsInterface, Loader } = require(dir.resolve([
+	"src",
+	"infrastructure",
+	"interfaces.js",
+]));
+const LangchainLoader = require(dir.resolve([
+	"src",
+	"infrastructure",
+	"Loaders",
+	"langchain-documents.js",
+]));
+dotenv.config();
+
+const inputFileURI = dir.resolve(
+	["extract", process.env.TEST_PDF_FILENAME],
+	__dirname
+);
+const expectedFileURI = dir.resolve(
+	["extract", process.env.TEST_EXPECTED_FILENAME],
+	__dirname
+);
+const resultFileURI = dir.resolve(
+	["extract", process.env.TEST_OUTPUT_FILENAME],
+	__dirname
+);
+const langchainLoader = new LangchainLoader();
+test("Check if interface is correctly implemented", () => {
+	expect(implementsInterface(langchainLoader, Loader)).toBe(true);
+});
+
+test("Extract test-document.pdf to raw text", async () => {
+	const expectedText = fs.readFileSync(expectedFileURI, "utf8").toString();
+	const result = await langchainLoader.readFile(inputFileURI);
+	await expect(result).toBe(expectedText);
+});
+test("Extract test-document.pdf to Document object", async () => {
+	const expectedText = fs.readFileSync(expectedFileURI, "utf8").toString();
+	const result = await langchainLoader.readFile(inputFileURI, true);
+	await expect(result.pageContent).toBe(expectedText);
+});
+
+test("Chunking text file from raw text to raw text array", async () => {
+	const text = "Lorem ipsum dolor sit amet.";
+	const expected = ["Lorem ipsum", "dolor sit", "sit amet."];
+	const result = await langchainLoader.chunk(text, 15, 5);
+	expect(result).toStrictEqual(expected);
+});
+
+test("Chunking text file from raw text to Document array", async () => {
+	const text = "Lorem ipsum dolor sit amet.";
+	const expected = ["Lorem ipsum", "dolor sit", "sit amet."];
+	const result = await langchainLoader.chunk(text, 15, 5, true);
+	expect(result.map((result) => result.pageContent)).toStrictEqual(expected);
+});
+
+test("Extract test-document.pdf to Document object and chunk it", async () => {
+	const expectedText = fs.readFileSync(expectedFileURI, "utf8").toString();
+	const doc = await langchainLoader.readFile(inputFileURI, true);
+	const chunked = await langchainLoader.chunk(doc, 15, 5, true);
+	const expected = [
+		"PDF de prueba",
+		"Texto de",
+		"de prueba.",
+		"Yolo",
+		"Yolo, Tengo",
+		"carácteres",
+		"extraños para",
+		"para todos los",
+		"los idiomas.",
+	];
+	console.log(chunked);
+	expect(chunked.map((chunk) => chunk.pageContent)).toStrictEqual(expected);
+});
