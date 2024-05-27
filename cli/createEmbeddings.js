8e0d66144bb13210f1a3c7d431f8e8e24a9a6c78
Yolvan G. Zambrano <yolvanzg@gmail.com>
2024-05-26 21:06:41 -0400

    Added more documents and a script to create the embeddings files by default due to floating point error when database is missing and cli.js is running.

---
+++ cli/createEmbeddings.js
@@ -0,0 +1,13 @@
+const app = require("../src/core/app.js");
+
+const main = async () => {
+	const loader = app.getFileLoader();
+	const model = app.getModels().getEmbeddingModel();
+	const embedding = await app.getEmbedding(loader, model);
+	console.log(embedding);
+};
+
+main().then(() => {
+	console.log("Embedding database created.");
+	process.exit();
+});
