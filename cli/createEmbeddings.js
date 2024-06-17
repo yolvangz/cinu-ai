7f4d60c894fc284637fa8bbd5f7ca8d0d81895c6
Yolvan G. Zambrano <yolvanzg@gmail.com>
2024-06-17 08:52:32 -0400

    Removed if exists remove from createEmbeddings

--- cli/createEmbeddings.js
+++ cli/createEmbeddings.js
@@ -1,9 +1,6 @@
-import { rm, access } from "node:fs/promises";
 import * as app from "../core/app.js";
 
 export default async function createEmbeddings () {
-	if (await access("./faiss_index").then(() => true).catch(() => false)) 
-		await rm("./faiss_index", { recursive: true });
 	const loader = app.getFileLoader();
 	const model = app.getModels().getEmbeddingModel();
 	const embedding = await app.getEmbedding(loader, model);
